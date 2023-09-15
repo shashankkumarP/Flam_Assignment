67a689675e01081d736dfc7616e06994c816eacf
Shashank Kumar <shashankkumar@Shashanks-MacBook-Air-2.local>
2023-09-16 00:08:48 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { useState } from 'react'
-import "./App.css"
-import BottomSheet from './component/BottomSheet'
+import { useState } from "react";
+import "./App.css";
+import BottomSheet from "./component/BottomSheet";
 
-const bodyStyle ={
+const bodyStyle = {
   width: "100%",
   height: "100%",
-  border:"10px solid",
-  backgroundColor: "rgba(7, 0, 0, 0.18)"
-}
+  border: "10px solid",
+  backgroundColor: "rgba(7, 0, 0, 0.18)",
+};
 
 function App() {
-  const [isModalOpen,setIsModalOpen] = useState(false);
-  
-  
-  
-  const handle_modal= (a)=>{
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handle_modal = (a) => {
     setIsModalOpen(a);
-  }
+  };
 
   return (
     <div style={isModalOpen ? bodyStyle : {}}>
-     <BottomSheet  handle_modal={handle_modal} />
-  </div>
-    
-  )
+      <h3>
+        React Bottom Sheet with Multiple Screen Snap Points and Spring Motion.
+        With vanilla CSS
+      </h3>
+      <BottomSheet handle_modal={handle_modal} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/component/BottomSheet.jsx
+++ src/component/BottomSheet.jsx
@@ -1,99 +1,81 @@
-import  { useState,useRef, useEffect,useCallback } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./BottomSheet.css";
 
-const BottomSheet = ({handle_modal}) => {
+const BottomSheet = ({ handle_modal }) => {
   const [position, setPosition] = useState("closed");
   const [isDragging, setIsDragging] = useState(false);
- let initialY = useRef(null);
- let delay=0;
- 
+  const [change, setChange] = useState(false);
+  let initialY = useRef(null);
+  let delay = 0;
+  let prev = 0;
+
   const handleSnap = (newPosition) => {
-    
-    if(newPosition=="half-open" || newPosition=="fully-open"){
-        handle_modal(true);
-    }else{
-        handle_modal(false);
+    if (newPosition == "half-open" || newPosition == "fully-open") {
+      handle_modal(true);
+    } else {
+      handle_modal(false);
     }
     setPosition(newPosition);
   };
-  
 
-   const handleMouseDown = (e) => {
-             setIsDragging(true);
-            initialY.current=e.pageY || e.touches?.[0].pageY;
-            
+  const handleMouseDown = (e) => {
+    setIsDragging(() => true);
+    setChange(() => true);
+    initialY.current = e.pageY || e.touches?.[0].pageY;
+    prev = e.pageY || e.touches?.[0].pageY;
+    console.log("isdragon", isDragging);
   };
-
-
 
   const handleMouseMove = (e) => {
-    console.log(e.clientY,initialY.current)
-    if (isDragging ) {
-    
-      delay =  initialY.current  - (e.pageY || e.touches?.[0].pageY);
-      console.log(delay,'mousemove');
+    console.log(e.clientY, initialY.current);
+    if (isDragging) {
+      delay = initialY.current - (e.pageY || e.touches?.[0].pageY);
+      prev = e.pageY || e.touches?.[0].pageY;
+      console.log(delay, "mousemove");
     }
   };
 
-  const handleMouseUp = () => {
-    console.log(delay,"up")
-    if (delay>=0 && position === "closed") {
-        handleSnap("half-open");
-        return;
-      } else if (delay >0 && position === "half-open") {
-        handleSnap("fully-open");
-        return;
-      }
-      if(delay<0 && position=="fully-open"){
-        handleSnap("half-open")
-      }else if(delay<0 && position=="half-open"){
-        handleSnap("closed")
-      }
-      
-  }
-
-  useEffect(()=>{
-
-    function handleMouseup(e){
-        // onGlobalMouseUp(e)
-        delay =  initialY.current  - (e.pageY || e.touches?.[0].pageY);
-        console.log('Mouse up event occurred:', e);
-        console.log(position)
-        if (delay>=0 && position === "closed") {
-            handleSnap("half-open");
-            return;
-          } else if (delay >0 && position === "half-open") {
-            handleSnap("fully-open");
-            return;
-          }
-          if(delay<0 && position=="fully-open"){
-            handleSnap("half-open")
-          }else if(delay<0 && position=="half-open"){
-            handleSnap("closed")
-          }
+  let handleMouseUp = (e) => {
+    console.log(delay, prev, "up", change);
+    if (!change) return;
+    if (delay == prev) return;
+    if (delay >= 0 && position === "closed") {
+      handleSnap("half-open");
+      return;
+    } else if (delay > 0 && position === "half-open") {
+      handleSnap("fully-open");
+      return;
     }
-    document.addEventListener('mouseup', handleMouseup);
+    if (delay < 0 && position == "fully-open") {
+      handleSnap("half-open");
+    } else if (delay < 0 && position == "half-open") {
+      handleSnap("closed");
+    }
+    setChange(() => false);
+  };
+
+  useEffect(() => {
+    function handleMouse(e) {
+      handleMouseUp(e);
+    }
+
+    document.addEventListener("mouseup", handleMouse);
+
     return () => {
-        document.removeEventListener('mouseup', handleMouseup);
-      };
-
-  },[position])
-
+      setChange(() => !change);
+      document.removeEventListener("mouseup", handleMouse);
+    };
+  }, [handleMouseUp]);
 
   return (
-    <div className={`bottom-sheet ${position} uparrow`}  onMouseDown={handleMouseDown}
-    onMouseMove={handleMouseMove}
-    // onMouseUp={()=>handleMouseUp()} 
-     >
+    <div
+      className={`bottom-sheet ${position} uparrow`}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+    >
       &#9650;
-      <div className="content">
-      </div>
-
-      
-      <div className="handle" 
-       >
-        Drag me upward and downward around this region
-      </div>
+      <div className="content"></div>
+      <div className="handle"></div>
       <div className="controls">
         <button onClick={() => handleSnap("closed")}>Close</button>
         <button onClick={() => handleSnap("half-open")}>Half</button>
